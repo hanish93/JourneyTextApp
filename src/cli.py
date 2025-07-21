c5c6b903a57d6fed5d2156ba8abc4e4737c520ac
hanish93 <110055903+hanish93@users.noreply.github.com>
2025-07-21 11:27:16 +0100

    Update cli.py

--- src/cli.py
+++ src/cli.py
@@ -1,12 +1,11 @@
 import argparse
 from app import run_pipeline
 
-if __name__ == "__main__":
-    p = argparse.ArgumentParser(description="Console journey summariser")
-    p.add_argument("--video", required=True,
-                   help="Path to a .mp4 file or directory")
+def main():
+    p=argparse.ArgumentParser(description="Console journey summariser")
+    p.add_argument("--video",required=True,
+                   help="Path to .mp4 file or directory")
     run_pipeline(p.parse_args().video)
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
