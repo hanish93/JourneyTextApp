b3d52c5c8035c807667ed69aef9b81e02bc088da
hanish93 <110055903+hanish93@users.noreply.github.com>
2025-07-28 00:32:08 +0100

    Update cli.py

--- src/cli.py
+++ src/cli.py
@@ -2,10 +2,12 @@ import argparse
 from .app import run_pipeline
 
 def main():
-    p=argparse.ArgumentParser(description="Journey summariser")
-    p.add_argument("-i","--input",required=True,
-                   help="Path to .mp4 video or folder of .jpg frames")
-    args=p.parse_args()
+    p = argparse.ArgumentParser(description="Journey summariser")
+    p.add_argument(
+        "-i","--input", required=True,
+        help="Path to .mp4 video or folder of .jpg frames"
+    )
+    args = p.parse_args()
     run_pipeline(args.input)
 
 if __name__=="__main__":
