9b6e34e5999e4f3d03d9bfa4d30acf2bffca48fe
hanish93 <110055903+hanish93@users.noreply.github.com>
2025-07-27 21:27:19 +0100

    Update cli.py

--- src/cli.py
+++ src/cli.py
@@ -6,15 +6,13 @@ if HERE not in sys.path:
 
 from app import run
 
-if __name__=="__main__":
+if __name__ == "__main__":
     import argparse, warnings, logging
     warnings.filterwarnings("ignore", category=UserWarning)
     logging.getLogger("ultralytics").setLevel(logging.ERROR)
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Journey CLI")
     parser.add_argument("-i","--input", required=True,
-                        help="Folder of JPG frames or MP4")
-    parser.add_argument("-m","--yolo-model", default=None,
-                        help="Custom YOLOv8 .pt (omit for yolov8n)")
+                        help="Folder of JPG frames or single MP4")
     args = parser.parse_args()
-    run(args.input, args.yolo_model)
+    run(args.input)
